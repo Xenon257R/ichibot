3a17b033bf873af33cf34426a5bd74edc4624ab4
Xenon257R <Xenon257R@gmail.com>
2023-09-17 10:22:59 -0700

    refactored code

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const client = new Client({
 	]
 });
 
-const setup = 'I have not been properly set up in this server. Use `-i` or `-init` so I am properly intialized!\nNOTE: You can re-run this command again in the future to reset server settings saved by this bot.';
+// String literal for when IchiBot is not set yet
+const setup = 'I have not been properly set up in this server. Use `-i`|`-init` so I can set up!\n' +
+	'NOTE: It is recommended to configure IchiBot using `-s`|`-set`|`-settings` for additional options. You can re-run `i`|`init` to reset these server settings.\n\n' +
+	'For a list of all commands and how to use them, type `-h`|`-help`.';
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
@@ -30,11 +33,15 @@ client.once(Events.ClientReady, c => {
 // Log in to Discord with your client's token
 client.login(token);
 
+// IchiBot listens to every message creation on server, and sees whether it shoulds respond to the message
 client.on('messageCreate', async message => {
+	// Returns if author is a bot
 	if (message.author.bot) return;
 
+	// Requests server details, or if they are not yet set
 	const server_info = await sqlitehandler.getServerDetails(message.guild.id);
 
+	// Exeption to server constraints to make sure IchiBot settings can always be configured
 	if (message.content.startsWith(prefix)) {
 		// CITATION: https://stackoverflow.com/questions/16261635/javascript-split-string-by-space-but-ignore-space-in-quotes-notice-not-to-spli
 		const args = message.content.slice(prefix.length).trim().match(/(?:[^\s"]+|"[^"]*")+/g);
@@ -45,72 +52,80 @@ client.on('messageCreate', async message => {
 			args[i] = args[i].replace(/^"(.+)"$/,'$1');
 		}
 
+		// Notifies user that server has not been initialized yet
 		if (!server_info.server_id) {
 			if (command != 'i' && command != 'init') return setup;
 		
 			return message.reply(await sqlitehandler.addServer(message.guild.id));
 		};
 
+		// Ignores messages if its #text_channel does not match server setting constraints
 		if (server_info.command_channel && message.channel.id != server_info.command_channel) return;
 
+		// Runs server-wide commands as provided
 		switch (command) {
 			case 'i':
 			case 'init':
+				// Initializes IchiBot
 				message.reply(await sqlitehandler.addServer(message.guild.id));
 				return;
 			case 's':
 			case 'set':
 			case 'settings':
+				// Change the settings of IchiBot on the server
 				switch (args[0]) {
 					case 'c':
 					case 'command':
+						// Change the #text_channel constraint for IchiBot Commands
 						if (!message.mentions.channels.at(0)) message.reply("You did not provide a channel to restrict commands.");
 						else message.reply(await sqlitehandler.modifyServer(message.guild.id, 'command_channel', message.mentions.channels.at(0).id));
 						return;
 					case 'p':
 					case 'player':
+						// Change the #player_channel constraint for IchiBot's embedded player
 						if (!message.mentions.channels.at(0)) message.reply("You did not provide a text channel to create the embed.");
 						else message.reply(await sqlitehandler.modifyServer(message.guild.id, 'player_channel', message.mentions.channels.at(0).id));
 						return;
 					case 'd':
 					case 'default':
+						// Change the option to use default music tracks of IchiBot in the server
 						if (!args[1]) {
 							message.reply("You did not provide any parameters for toggling default music sesttings.");
 							break;
 						}
 						switch(args[1]) {
-							case 0:
-							case '0':
 							case 'false':
-							case 'disable':
 								message.reply(await sqlitehandler.modifyServer(message.guild.id, 'enable_default', false));
 								break;
-							case 1:
-							case '1':
 							case 'true':
-							case 'enable':
 								message.reply(await sqlitehandler.modifyServer(message.guild.id, 'enable_default', true));
 								break;
 							default:
-								message.reply("You did not provide a valid parameter for toggling default music settings.");
+								message.reply("You did not provide true or flase for sdefault music settings.");
 						}
 						return;
 					default:
+						// An incorrect command was provided, or lack thereof
 						if (args.length <= 0) message.reply("You did not provide any arguments for changing bot settings.");
 						else message.reply("You did not provide a valid argument for changing bot settings.");
 				}
 				return;
 			case 'h':
 			case 'help':
+				// Requests help from IchiBot on how to use it
+
 				message.reply(helpdoc.specificHelp(args[0]));
 				return;
 		}
 
+		// Sets #command_channel to message's #channel if no #command_channel is set on server
 		if (!server_info.command_channel) server_info.command_channel = message.channel.id;
 
+		// Runs commands as provided
 		switch (command) {
 			case 'p':
 			case 'profile':
+				// Edits user profile on server
 				if (args.length < 1) {
 					message.reply("You did not provide enough arguments: `-p|profile [image_url|d|delete]`");
 					break;
@@ -120,6 +135,7 @@ client.on('messageCreate', async message => {
 				break;
 			case 'a':
 			case 'add':
+				// Adds specified track to server
 				if (args.length < 3) {
 					message.reply("You did not provide enough arguments: `-a|add [type] [track_name] [track_url]`");
 					break;
@@ -129,13 +145,13 @@ client.on('messageCreate', async message => {
 					case 'hanchan':
 					case '0':
 					case 0:
-						message.reply(await sqlitehandler.addTrack(message.author.id, message.guild.id, args[1], args[2], 0));
+						message.reply(await sqlitehandler.addTrack(message.guild.id, message.author.id, args[1], args[2], 0));
 						break;
 					case 'r':
 					case 'riichi':
 					case '1':
 					case 1:
-						message.reply(await sqlitehandler.addTrack(message.author.id, message.guild.id, args[1], args[2], 1));
+						message.reply(await sqlitehandler.addTrack(message.guild.id, message.author.id, args[1], args[2], 1));
 						break;
 					default:
 						message.reply("Invalid track type provided. Valid types are `h`/`hanchan`/`0` or `r`/`riichi`/`1`.");
@@ -144,23 +160,51 @@ client.on('messageCreate', async message => {
 				break;
 			case 'd':
 			case 'delete':
+				// Deletes specified track from server
 				if (args. length < 1) {
 					message.reply("You did not provide enough arguments: `-d|delete [track_name]");
 					break;
 				}
-				message.reply(await sqlitehandler.removeTrack(message.author.id, message.guild.id, args[0]));
+				message.reply(await sqlitehandler.removeTrack(message.guild.id, message.author.id, args[0]));
+				break;
+			case 'f':
+			case 'force':
+				// Force playback from music player when its active
+				message.reply(await voicehandler.forcePlay(message.guild.id, message.mentions.users.at(0), args[0]));
+				break;
+			case 'l':
+			case 'list':
+				// Lists user's album on the server
+				const allUploads = await sqlitehandler.listUploads(message.guild.id, message.author.id);
+				message.reply('Here is a list of all the tracks you put in this server!');
+				let chunk = '```asciidoc\n';
+				if (allUploads.length <= 0) chunk = chunk + '[Empty]';
+				for(let i = 0; i < allUploads.length; i++) {
+					if (chunk.length > 1750) {
+						client.channels.cache.get(server_info.command_channel).send(chunk + '```');
+						chunk = '```asciidoc\n';
+					}
+					chunk = chunk + (allUploads[i].type === 0 ? 'hnc' : 'rch') + ' :: ' + allUploads[i].track_name + '\n';
+				}
+				client.channels.cache.get(server_info.command_channel).send(chunk + '```');
 				break;
 			default:
+				// Invalid command was provided
 				message.reply("Invalid command. Type -help to get a list of commands that IchiBot can use.");
 		}
 	}
+
+	// Handles @IchiBot mention
 	else if (message.mentions.has(client.user) && !message.mentions.everyone) {
 		if (!server_info.server_id) {
 			message.reply(setup);
 			return;
 		}
+
+		// Sets #player_channel to message's #channel if no #player_channel is set on server
 		if (!server_info.player_channel) server_info.player_channel = message.channel.id;
 
+		// Prompts IchiBot to join voice channel of user and handles failures accordingly
 		const channel = message.member?.voice.channel;
 		if (channel) {
 			try {
